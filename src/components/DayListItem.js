480ede56347269dda7f52ff3e1a70bac217de3a6
Matt Wardle <mwardle.22@gmail.com>
2019-11-25 13:11:29 -0500

    Complete Listing Days II

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -9,10 +9,22 @@ export default function DayListItem({ name, spots, selected, setDay }) {
     "say-list__item--full": !spots
   })
 
+  const formatSpots = (spots) => {
+    if (spots === 0) {
+      return "no spots remaining"
+    } else if (spots === 1) {
+      return "1 spot remaining"
+    } else if (spots > 1) {
+      return `${spots} spots remaining`
+    } else {
+      return "An error has occured."
+    }
+  }
+
   return (
     <li className={dayClass} onClick={() => setDay(name)}>
       <h2 className="text--regular">{name}</h2>
-      <h3 className="text--light">{spots}</h3>
+      <h3 className="text--light">{formatSpots(spots)}</h3>
     </li>
   );
 }
