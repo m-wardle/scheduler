83f1eae84bde7f1ae83bc4eff492623061d126d1
Matt Wardle <mwardle.22@gmail.com>
2019-11-25 19:26:17 -0500

    Complete Displaying Appointments and The Schedule

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,8 +1,8 @@
 import React, { useState } from "react";
-
 import "components/Application.scss";
-import "components/DayList"
+
 import DayList from "components/DayList";
+import Appointment from "components/Appointment";
 
 const days = [
   {
@@ -20,6 +20,69 @@ const days = [
     name: "Wednesday",
     spots: 0,
   },
+];
+
+const appointments = [
+  {
+    id: 1,
+    time: "12pm",
+  },
+  {
+    id: 2,
+    time: "1pm",
+    interview: {
+      student: "Lydia Miller-Jones",
+      interviewer: {
+        id: 1,
+        name: "Sylvia Palmer",
+        avatar: "https://i.imgur.com/LpaY82x.png",
+      }
+    }
+  },
+  {
+    id: 3,
+    time: "2pm",
+    interview: {
+      student: "Angela Abar",
+      interviewer: {
+        id: 2,
+        name: "Tori Malcom",
+        avatar: "https://i.imgur.com/Nmx0Qxo.png",
+      }
+    }
+  },
+  {
+    id: 4,
+    time: "3pm",
+  },
+  {
+    id: 5,
+    time: "4pm",
+    interview: {
+      student: "John Osterman",
+      interviewer: {
+        id: 3,
+        name: "Mildred Nazir",
+        avatar: "https://i.imgur.com/T2WwVfS.png",
+      }
+    }
+  },
+  {
+    id: 6,
+    time: "5pm",
+  },
+  {
+    id: 7,
+    time: "6pm",
+    interview: {
+      student: "William Reeves",
+      interviewer: {
+        id: 3,
+        name: "Cohana Roy",
+        avatar: "https://i.imgur.com/FK8V841.jpg",
+      }
+    }
+  }
 ];
 
 export default function Application(props) {
@@ -49,6 +112,14 @@ export default function Application(props) {
       </section>
       <section className="schedule">
         {/* Replace this with the schedule elements durint the "The Scheduler" activity. */}
+        {appointments.map(appointment => {
+          return (
+            <Appointment
+              key={appointment.id}
+              {...appointment}
+            />
+          )
+        })}
       </section>
     </main>
   );
