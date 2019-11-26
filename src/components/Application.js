a6d829459abfd07794fa494b2eec6f579a52e39e
Matt Wardle <mwardle.22@gmail.com>
2019-11-26 15:45:54 -0500

    Implement sourcing appointment data from api

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,72 +1,10 @@
 import React, { useState, useEffect } from "react";
 import Axios from "axios";
+import { getAppointmentsForDay } from "helpers/selectors";
 import "components/Application.scss";
 
 import DayList from "components/DayList";
 import Appointment from "components/Appointment";
-
-const appointments = [
-  {
-    id: 1,
-    time: "12pm",
-  },
-  {
-    id: 2,
-    time: "1pm",
-    interview: {
-      student: "Lydia Miller-Jones",
-      interviewer: {
-        id: 1,
-        name: "Sylvia Palmer",
-        avatar: "https://i.imgur.com/LpaY82x.png",
-      }
-    }
-  },
-  {
-    id: 3,
-    time: "2pm",
-    interview: {
-      student: "Angela Abar",
-      interviewer: {
-        id: 2,
-        name: "Tori Malcom",
-        avatar: "https://i.imgur.com/Nmx0Qxo.png",
-      }
-    }
-  },
-  {
-    id: 4,
-    time: "3pm",
-  },
-  {
-    id: 5,
-    time: "4pm",
-    interview: {
-      student: "John Osterman",
-      interviewer: {
-        id: 3,
-        name: "Mildred Nazir",
-        avatar: "https://i.imgur.com/T2WwVfS.png",
-      }
-    }
-  },
-  {
-    id: 6,
-    time: "5pm",
-  },
-  {
-    id: 7,
-    time: "6pm",
-    interview: {
-      student: "William Reeves",
-      interviewer: {
-        id: 3,
-        name: "Cohana Roy",
-        avatar: "https://i.imgur.com/FK8V841.jpg",
-      }
-    }
-  }
-];
 
 export default function Application(props) {
   const [state, setState] = useState({
@@ -75,13 +13,20 @@ export default function Application(props) {
     appointments: {}
   })
 
+  const appointments = getAppointmentsForDay(state, state.day)
+
   const setDay = day => setState(prev => ({ ...prev, day }));
-  const setDays = days => setState(prev => ({ ...prev, days }));
 
   useEffect(() => {
-    Axios.get("/api/days")
-      .then(res => setDays(res.data))
+    Promise.all([
+      Axios.get("/api/days"),
+      Axios.get("api/appointments")
+    ]).then((all) => {
+      setState(prev => ({ ...prev, days: all[0].data, appointments: all[1].data }))
+    })
       .catch(err => console.log(err))
+
+
   }, [])
 
   return (
