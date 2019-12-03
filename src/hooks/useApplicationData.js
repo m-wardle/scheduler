41fd407bf990ff1a847b2a711898ec2f14090ccb
Matt Wardle <mwardle.22@gmail.com>
2019-12-02 21:41:58 -0500

    Move reducer to its own file for testing. Implement test for error handling. Exclude unnecessary coverage indexing.

--- src/hooks/useApplicationData.js
+++ src/hooks/useApplicationData.js
@@ -1,66 +1,6 @@
 import React, { useReducer, useEffect } from "react";
 import axios from "axios";
-
-const SET_DAY = "SET_DAY";
-const SET_APPLICATION_DATA = "SET_APPLICATION_DATA";
-const SET_INTERVIEW = "SET_INTERVIEW";
-
-function reducer(state, action) {
-  switch (action.type) {
-    case SET_DAY:
-      return {
-        ...state,
-        day: action.day,
-      }
-    case SET_APPLICATION_DATA:
-      return {
-        ...state,
-        days: action.days,
-        appointments: action.appointments,
-        interviewers: action.interviewers
-      }
-    case SET_INTERVIEW:
-      let changeDay;
-
-      if (1 <= action.id && action.id <= 5) {
-        changeDay = 0
-      } else if (6 <= action.id && action.id <= 10) {
-        changeDay = 1
-      } else if (11 <= action.id && action.id <= 15) {
-        changeDay = 2
-      } else if (16 <= action.id && action.id <= 20) {
-        changeDay = 3
-      } else if (21 <= action.id && action.id <= 25) {
-        changeDay = 4
-      }
-
-      let dayArr = [...state.days];
-      console.log(state.appointments[action.id].interview)
-
-      dayArr[changeDay].spots = (action.interview ?
-        (state.appointments[action.id].interview ?
-          dayArr[changeDay].spots :
-          dayArr[changeDay].spots - 1) :
-        dayArr[changeDay].spots + 1)
-
-
-      return {
-        ...state,
-        appointments: {
-          ...state.appointments,
-          [action.id]: {
-            ...state.appointments[action.id],
-            interview: action.interview
-          }
-        },
-        days: [...dayArr]
-      }
-    default:
-      throw new Error(
-        `Tried to reduce with unsupported action type: ${action.type}`
-      )
-  }
-}
+import reducer, { SET_DAY, SET_APPLICATION_DATA, SET_INTERVIEW } from "../reducers/application"
 
 export default function useApplicationData() {
 
